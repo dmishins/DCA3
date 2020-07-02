1a4ca17e68a75e76b4d8e37722b0b24ea579952a
Alexis Metzler <metzlera@umich.edu>
2020-07-02 11:13:54 -0400

    changed variable names to be more intuitive

--- ADC_to_uA.py
+++ ADC_to_uA.py
@@ -1,16 +1,16 @@
-def ADC_to_uA(V, g_exp):
+def ADC_to_uA(A0, gain_exp):
     res = -1000.0
-    if (V >4.096):
-        V = V-8.192
+    if (A0 >4.096):
+        A0 = A0-8.192
 
-    if (abs(V) < 1):
+    if (abs(A0) < 1):
         gain_adj = 1
-    elif (abs(V) > 4):
+    elif (abs(A0) > 4):
         gain_adj = -1
     else:
         gain_adj = 0
 
-    V = V * (2 ** g_exp)
-    res = V / 0.004
-    correct_V = V - res / 250
-    return res , correct_V , gain_adj
+    A0 = A0 * (2 ** gain_exp)
+    res = A0 / 0.004
+    correct_A0 = A0 - res / 250
+    return res , correct_A0 , gain_adj
